b5c718c4cf12f36a691946e2b5954c5f1cbc33e0
Craig Peterson <craig.peterson0701@gmail.com>
2025-02-18 12:44:20 -0600

    shortened import names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import column_names
+import column_names as col
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -23,15 +23,15 @@ def format_courses_taught(courses: str) -> list[str]:
 
 def print_preferences():
     for idx, row in grader_preferences.iterrows():
-       print(f"idx: {idx}, Grader: {row[column_names.f1_grader_name]}, Advisor: {row[column_names.f1_advisor]}, Course Preferences: {row[column_names.f1_course_pref]}")
+       print(f"idx: {idx}, Grader: {row[col.f1_grader_name]}, Advisor: {row[col.f1_advisor]}, Course Preferences: {row[col.f1_course_pref]}")
 
 def print_info():
     for idx, row in grader_info.iterrows():
-        print(f"idx: {idx}, Grader: {row[column_names.f2_grader_name]}, Class Times: {row[column_names.f2_class_times]}, Courses: {row[column_names.f2_grader_courses]}, Taught Courses: {format_courses_taught(row[column_names.f2_taught_courses])}, Notes: {row[column_names.f2_notes]}")
+        print(f"idx: {idx}, Grader: {row[col.f2_grader_name]}, Class Times: {row[col.f2_class_times]}, Courses: {row[col.f2_grader_courses]}, Taught Courses: {format_courses_taught(row[col.f2_taught_courses])}, Notes: {row[col.f2_notes]}")
 
 def print_classes():
     for idx, row in classes.iterrows():
-        print(f"idx: {idx}, Class: {row[column_names.f3_crn]}, Department: {row[column_names.f3_department]}, Course Name: {row[column_names.f3_course_title]}, Days: {row[column_names.f3_days]}, Time: {row[column_names.f3_times]}")
+        print(f"idx: {idx}, Class: {row[col.f3_crn]}, Department: {row[col.f3_department]}, Course Name: {row[col.f3_course_title]}, Days: {row[col.f3_days]}, Time: {row[col.f3_times]}")
 
 if __name__ == "__main__":
     print_preferences()
