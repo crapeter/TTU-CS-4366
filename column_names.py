ac7b32de9103a6327fc6d3c5d783f15b0a305b5d
Craig Peterson <craig.peterson0701@gmail.com>
2025-02-18 09:53:33 -0600

    initial setup

---
+++ column_names.py
@@ -0,0 +1,82 @@
+''' Column Names
+File1-Spring-2025-TA-Graders-PhD_Master-information
+['Unnamed: 0',
+ 'Headcount',
+ 'FTE count',
+ 'Applicant Name (first name last name) ',
+ 'Research Advisor\'s Name (first name last name) ',
+ 'Do you have TA Experience in CS department at TTU? If yes, when?',
+ 'List the courses that you want to be a TA in the order from highest preference to lowest. For each course you listed, you must have taken it before and include your grade. Example of format: 5383(A), 6345(B). ''before and include your grade. Example of format: 5383(A), 6345(B). ']
+'''
+f1_headcount = 'Headcount'
+f1_FTE = 'FTE count'
+f1_grader_name = 'Applicant Name (first name last name) '
+f1_advisor = 'Research Advisor\'s Name (first name last name) '
+f1_grader_exp = 'Do you have TA Experience in CS department at TTU? If yes, when?'
+f1_course_pref = 'List the courses that you want to be a TA in the order from highest preference to lowest. For each course you listed, you must have taken it before and include your grade. Example of format: 5383(A), 6345(B). '
+
+
+''' Column Names
+File2-TA_Grader Avail and Preferences and special request from instrutors - 2025
+['TA grader name ',
+ 'Time/day of the class TA/grader has to attend in spring 2025',
+ 'Course you are taking spring 2025 ',
+ 'course you have taught (as TA/grader/GPTI)\nSemester + course code',
+ 'Notes']
+'''
+f2_grader_name = 'TA grader name '
+f2_class_times = 'Time/day of the class TA/grader has to attend in spring 2025'
+f2_grader_courses = 'Course you are taking spring 2025 '
+f2_taught_courses = 'course you have taught (as TA/grader/GPTI)\nSemester + course code'
+f2_notes = 'Notes'
+
+
+''' Column Names
+File3-Open_Classes_Report_01-10-2025_09.19.41_PM
+['CRN',
+ 'Subject Title',
+ 'Subject Code',
+ 'Course Number',
+ 'Section Number',
+ 'Course Title',
+ 'Days',
+ 'Times',
+ 'Start Date',
+ 'End Date',
+ 'Campus',
+ 'Building',
+ 'Room Number',
+ 'Instructor',
+ 'Max Enrollment',
+ 'Current Enrollment',
+ 'Seats Available',
+ 'Wait Capacity',
+ 'Wait Count',
+ 'Wait Available',
+ 'Section Type',
+ 'Available for Visiting Students',
+ 'Linked To']
+'''
+f3_crn = 'CRN'
+f3_department = 'Subject Title'
+f3_subject_code = 'Subject Code'
+f3_course_number = 'Course Number'
+f3_section_number = 'Section Number'
+f3_course_title = 'Course Title'
+f3_days = 'Days'
+f3_times = 'Times'
+f3_start_date = 'Start Date'
+f3_end_date = 'End Date'
+f3_campus = 'Campus'
+f3_building = 'Building'
+f3_room_number = 'Room Number'
+f3_instructor = 'Instructor'
+f3_max_enrollment = 'Max Enrollment'
+f3_current_enrollment = 'Current Enrollment'
+f3_seats_available = 'Seats Available'
+f3_wait_capacity = 'Wait Capacity'
+f3_wait_count = 'Wait Count'
+f3_wait_available = 'Wait Available'
+f3_section_type = 'Section Type'
+f3_available_for_visiting_students = 'Available for Visiting Students'
+f3_linked_to = 'Linked To'
--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import pandas as pd
+import column_names
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-df = pd.read_excel("C:\\Users\\Owner\\OneDrive\\Capstone\\Capstone Input Files\\File1-Spring-2025-TA-Graders-PhD_Master-information.xlsx")
+# f1_
+grader_preferences = pd.read_excel(os.getenv('FILE1')).dropna(how="all")
+
+# f2_
+grader_info = pd.read_excel(os.getenv('FILE2')).dropna(how="all")
+
+# f3_
+classes = pd.read_excel(os.getenv('FILE3')).dropna(how="all")
+
+def format_courses_taught(courses: str) -> list[str]:
+    return [course.strip() for course in courses.split("\n") if course.strip()]
+
+def print_preferences():
+    for idx, row in grader_preferences.iterrows():
+       print(f"idx: {idx}, Grader: {row[column_names.f1_grader_name]}, Advisor: {row[column_names.f1_advisor]}, Course Preferences: {row[column_names.f1_course_pref]}")
+
+def print_info():
+    for idx, row in grader_info.iterrows():
+        print(f"idx: {idx}, Grader: {row[column_names.f2_grader_name]}, Class Times: {row[column_names.f2_class_times]}, Courses: {row[column_names.f2_grader_courses]}, Taught Courses: {format_courses_taught(row[column_names.f2_taught_courses])}, Notes: {row[column_names.f2_notes]}")
+
+def print_classes():
+    for idx, row in classes.iterrows():
+        print(f"idx: {idx}, Class: {row[column_names.f3_crn]}, Department: {row[column_names.f3_department]}, Course Name: {row[column_names.f3_course_title]}, Days: {row[column_names.f3_days]}, Time: {row[column_names.f3_times]}")
 
 if __name__ == "__main__":
-    print("This is the main module.")
-    print(df.head())
+    print_preferences()
+    print()
+    print_info()
+    print()
+    print_classes()
